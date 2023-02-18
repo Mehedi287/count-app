e3673558581b8f7f50eaf20c99b14a0bc82f4b75
Mehedi390 <mh0816150@gmail.com>
2023-02-18 02:56:59 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Count from "./Components/Count";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        <Count />
+        <Count />
+        <Count />
+      </div>
+      <div className="mx-auto bg-white mx-w-md space-y-5 w-52">
+        <h1 className="font-bold text-center text-3xl m-3 p-3 ">
+          total count = 0
+        </h1>
+      </div>
     </div>
   );
 }
